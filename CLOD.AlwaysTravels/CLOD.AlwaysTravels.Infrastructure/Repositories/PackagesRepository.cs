3490308485bd9436e10600f52cc04096c0b5bfdb
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-07-17 15:31:55 +0200

    feat(infrastructure) : added infrastructure layer

---
+++ CLOD.AlwaysTravels/CLOD.AlwaysTravels.Infrastructure/Repositories/PackagesRepository.cs
@@ -0,0 +1,52 @@
+﻿using CLOD.AlwaysTravels.AppCore.Interfaces.Repositories;
+using CLOD.AlwaysTravels.Domain;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using RepoDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLOD.AlwaysTravels.Infrastructure.Repositories
+{
+    public class PackagesRepository : IPackagesRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public PackagesRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<IEnumerable<Package>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Package> GetAsync(int id)
+        {
+            var cs = _configuration.GetConnectionString("AlwaysTravelsDB");
+            using var DB = new NpgsqlConnection(cs);
+            return (await DB.QueryAsync<Package>(p => p.Id == id)).FirstOrDefault();
+        }
+
+        public async Task<int> InsertAsync(Package model)
+        {
+            var cs = _configuration.GetConnectionString("AlwaysTravelsDB");
+            using var DB = new NpgsqlConnection(cs);
+            return await DB.InsertAsync<Package, int>(model);
+        }
+
+        public Task<int> UpdateAsync(Package model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
